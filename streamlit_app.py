ec368b40905a52b34ed647e050fdad5648b140d0
Tom <121887007+tomvsnow@users.noreply.github.com>
2023-01-04 12:52:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# streamlit.dataframe(my_fruit_list)
+streamlit.text('🍌🥭Available Fruit🥝🍇')
+streamlit.dataframe(my_fruit_list)
 
 
 
